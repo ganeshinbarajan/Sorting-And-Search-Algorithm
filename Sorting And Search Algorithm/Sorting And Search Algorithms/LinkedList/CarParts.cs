5824876e0d8490823f29c8200c23b7ddc4528022
ganeshinbarajan <ganesh.inbarajan@gmail.com>
2016-05-22 20:36:53 -0700

    ok

---
+++ Sorting And Search Algorithm/Sorting And Search Algorithms/LinkedList/CarParts.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sorting_And_Search_Algorithms.LinkedList
+{
+    class CarPart
+    {
+        public long PartNumber;
+        public string PartName;
+        public double UnitPrice;
+        public CarPart Next;
+    };
+
+    class LinkedList
+    {
+        private int size;
+        private CarPart Head;
+
+        public LinkedList()
+        {
+            size = 0;
+            Head = null;
+        }
+
+        public int Count
+        {
+            get { return size; }
+        }
+
+        
+
+        public int Add(CarPart NewItem)
+        {
+            CarPart Sample = new CarPart();
+
+            Sample = NewItem;
+            Sample.Next = Head;
+            Head = Sample;
+            return size++;
+        }
+
+        public CarPart Retrieve(int Position)
+        {
+            CarPart Current = Head;
+
+            for (int i = 0; i < Position && Current != null; i++)
+                Current = Current.Next;
+            return Current;
+        }
+
+        public bool Delete()
+        {
+            if (Head == null)
+            {
+                Console.WriteLine("The list is empty");
+                return false;
+            }
+
+            CarPart Current;
+
+            Current = Head.Next;
+            Head.Next = Current.Next;
+            size--;
+            return true;
+        }
+        public bool Delete(int Position)
+        {
+            if (this.Retrieve(Position) == null)
+                return false;
+
+            this.Retrieve(Position - 1).Next = this.Retrieve(Position + 1);
+            size--;
+            return true;
+        }
+        public bool Find(CarPart ItemToFind)
+        {
+            CarPart Current = new CarPart();
+
+            if (ItemToFind == null)
+                return false;
+
+            for (Current = Head; Current != null; Current = Current.Next)
+            {
+                if ((Current.PartNumber == ItemToFind.PartNumber) &&
+                    (Current.PartName == ItemToFind.PartName) &&
+                    (Current.UnitPrice == ItemToFind.UnitPrice))
+                    return true;
+            }
+
+            return false;
+        }
+
+    }
+}
--- Sorting And Search Algorithm/Sorting And Search Algorithms/Program.cs
+++ Sorting And Search Algorithm/Sorting And Search Algorithms/Program.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Sorting_And_Search_Algorithms.Sorting;
 using Sorting_And_Search_Algorithms.Searching;
+using Sorting_And_Search_Algorithms.LinkedList;
 
 namespace Sorting_And_Search_Algorithms
 {
@@ -16,13 +17,71 @@ namespace Sorting_And_Search_Algorithms
             //var bubbleSort = new BubbleSort();
             //var result = bubbleSort.Sorting(input);
 
-            var quickSort = new QuickSort();
-            var quickSortResult=quickSort.Sort(input);
+            //var quickSort = new QuickSort();
+            //var quickSortResult=quickSort.Sort(input);
 
-            var binarySearch = new Binary();
-            var binarySearchResult = binarySearch.Search(quickSortResult, 5);
-            Console.Write(input);
-            Console.ReadLine();
+            //var binarySearch = new Binary();
+            //var binarySearchResult = binarySearch.Search(quickSortResult, 5);
+            //Console.Write(input);
+            //Console.ReadLine();
+            TestLinkedList();
+        }
+        static void TestLinkedList()
+        {
+            LinkedList.LinkedList Parts = new LinkedList.LinkedList();
+            CarPart Part;
+            CarPart PartToFind;
+
+            Part = new CarPart();
+            Part.PartNumber = 9743;
+            Part.PartName = "Air Filter";
+            Part.UnitPrice = 8.75;
+            Parts.Add(Part);
+            
+            Part = new CarPart();
+            Part.PartNumber = 27487;
+            Part.PartName = "Clutch Disk";
+            Part.UnitPrice = 47.15;
+            Parts.Add(Part);
+
+            Part = new CarPart();
+            Part.PartNumber = 87873;
+            Part.PartName = "Brake Disk";
+            Part.UnitPrice = 35.15;
+            Parts.Add(Part);
+
+            Part = new CarPart();
+            Part.PartNumber = 27644;
+            Part.PartName = "A/C Filter Drier";
+            Part.UnitPrice = 55.55;
+            Parts.Add(Part);
+
+            Console.WriteLine("Number of Parts: {0}", Parts.Count);
+
+            for (int i = 0; i < Parts.Count; i++)
+            {
+                CarPart part = Parts.Retrieve(i);
+                Console.WriteLine("\nCar Part Information");
+                Console.WriteLine("Part #:      {0}", part.PartNumber);
+                Console.WriteLine("Description: {0}", part.PartName);
+                Console.WriteLine("Unit Price:  {0:C}", part.UnitPrice);
+            }
+            //Parts.Delete(2);
+            PartToFind = new CarPart();
+            PartToFind.PartNumber = 87873;
+            PartToFind.PartName = "Brake Disk";
+            PartToFind.UnitPrice = 35.15;
+
+            bool Found = Parts.Find(PartToFind);
+            if (Found == true)
+                Console.WriteLine("\nItem was found\n");
+            else
+                Console.WriteLine("\nItem not found\n");
+
+            Console.WriteLine();
+
+
+            //return 0;
         }
     }
 }
